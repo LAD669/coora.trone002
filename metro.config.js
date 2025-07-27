54751e36286305367e4973ab2026665fd49938d2
LAD667 <load.delessert@coora.app>
2025-07-27 11:54:50 +0200

    Fix: Entferne web-Plattform aus Metro-Konfig

--- metro.config.js
+++ metro.config.js
@@ -3,8 +3,8 @@ const { getDefaultConfig } = require('expo/metro-config');
 module.exports = (async () => {
   const config = await getDefaultConfig(__dirname);
 
-  // Ensure proper handling of environment variables
-  config.resolver.platforms = ['ios', 'android', 'native', 'web'];
+  // Nur mobile Plattformen aktiv
+  config.resolver.platforms = ['ios', 'android', 'native'];
 
   return config;
 })();
